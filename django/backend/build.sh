51257125c20431c578110b5df8c9e017adc1c06f
Justin Chung <0921justin@gmail.com>
2026-05-05 14:30:24 -0400

    cd to correct directory before running BE on Render

--- django/backend/build.sh
+++ django/backend/build.sh
@@ -2,6 +2,9 @@
 # exit on error
 set -o errexit
 
+# Render runs this from the repo root, so we must CD into the backend folder first
+cd django/backend
+
 pip install --upgrade pip
 pip install -r requirements.txt
 
